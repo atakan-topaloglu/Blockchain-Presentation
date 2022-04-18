bd87e04e5dcb244c55f8784056737ce98f3f6253
Atakan-Topaloglu <atakantopaloglu@gmail.com>
2022-04-18 15:40:38 +0300

    Improved interface that allows for more interaction to encode and decode messages

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from encrypter_decrypter import encrypt, decrypt
 # Some useful four digit prime numbers:
 # 8999, 1949, 9467, 2003, 3347
 
-
 def main():
 
     keys_generated = False 
@@ -21,27 +20,31 @@ def main():
 
         if choice == 'G':
             n, phi = calculate_n_and_phi()
-            e, n = generate_public_key(n, phi)
-            d, n = generate_private_key(e, n, phi)
+            public_key_1, n = generate_public_key(n, phi)
+            private_key_1, n = generate_private_key(public_key_1, n, phi)
             keys_generated = True 
             print('Keys have been generated') 
-            print('Public key: (', e, ', ',  n, ')', sep='')
-            print('Private key: (', d, ', ', n, ')', '\n', sep='')
+            print('Public key: (', public_key_1, ', ',  n, ')', sep='')
+            choice_priv_key = str(input("Please type 'Yes' to access the private key \n"))
+            if choice_priv_key.lower() == "yes":
+                print('Private key: (', private_key_1, ', ', n, ')', '\n', sep='')
 
         elif choice == 'E':
             if not keys_generated: 
                 print('You need to generate the keys before encryption\n') 
             else: 
-                x = int(input('Enter the message to be encrypted: ')) 
-                y = encrypt(x, e, n)
+                x = int(input('Enter the message to be encrypted: '))
+                public_key_input = int(input("Please enter the first element of public key: "))
+                y = encrypt(x, public_key_input, n)
                 print(x, 'is encrypted as', y, '\n')
 
         elif choice == 'D':
             if not keys_generated: 
                 print('You need to generate the keys before decryption', '\n') 
             else: 
-                y = int(input('Enter the message to be decrypted: ')) 
-                x = decrypt(y, d, n)
+                y = int(input('Enter the message to be decrypted: '))
+                private_key_input = int(input("Please enter the first element of private key: "))
+                x = decrypt(y, private_key_input, n)
                 print(y, 'is decrypted as', x, '\n')
         choice = display_menu()
 
